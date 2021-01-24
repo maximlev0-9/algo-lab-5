746cb003be486d8f5c2aaf2192b781df252c491f
Max <maximlev01234@gmail.com>
2021-01-24 17:25:56 +0200

    updated game of life program

--- game_of_life.py
+++ game_of_life.py
@@ -1,14 +1,22 @@
+# v1 of fix: add 1 row and col to each side of cells array, and after each iteration remove unnecessary
+
+# there is also another variant of fix - for all edge rows and cols of cell check: 
+# if there are 3 cells in a row - add new row/column (if not already) and set 1 where it is needed 
+# (in front of central cell in that row)
+
+
 def get_generation(cells, generations):
+    cells.insert(0, [0 for i in range])
     result = [[0 for i in range(len(cells[0]))] for i in range(len(cells))]
-    rows = len(cells)
-    cols = len(cells[0])
+    rows = len(cells)+2
+    cols = len(cells[0])+2
     for iteration in range(generations):
         for row in range(rows):
             for col in range(cols):
                 neighbour_cells = get_neighbour_cells(row, col, rows, cols)
                 # print("generation:", iteration)
-                print(row, col)
-                print(neighbour_cells)
+                # print(row, col)
+                # print(neighbour_cells)
                 count = 0
                 for i, j in neighbour_cells:
                     if cells[i][j]:
@@ -68,3 +76,9 @@ def main():
 if __name__ == '__main__':
     main()
     # TODO: make input from console and any number of iterations
+    # TODO: curr mistake is that the array should expand itself if needed 
+    # (for example, in 0 0 1
+    #                  0 1 1
+    #                  0 0 1)
+    # there should be created a new cell to the right and also array should be cropped to the left of this array 
+
